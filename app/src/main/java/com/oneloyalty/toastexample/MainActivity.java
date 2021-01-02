325c807e03bd42b3ec2c573f06bd450f80195027
ark-ubn-3 <manoj@in.arokiait.com>
2021-01-02 12:09:40 +0530

    android Toast Library

---
+++ app/src/main/java/com/oneloyalty/toastexample/MainActivity.java
@@ -0,0 +1,18 @@
+package com.oneloyalty.toastexample;
+
+import androidx.annotation.LongDef;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        LogDebug.d("aslam");
+
+    }
+}
